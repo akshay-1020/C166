c9ea247d083d75b3f414d64d38b1d81ffa54ac8e
akshay-1020 <88386298+akshay-1020@users.noreply.github.com>
2023-06-07 17:01:00 -0500

    Add files via upload

--- ADV-C166 DRAWING SHAPES ON CANVAS.py
+++ ADV-C166 DRAWING SHAPES ON CANVAS.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 
-# I tried this project, the place function doesn't work for some reason. I get the concept, though.
-
 root = Tk()
 root.geometry("1000x800")
 root.title("Scibbled Canvas")
@@ -18,8 +16,26 @@ colorcombo.pack()
 
 startxlbl = Label(root, text="Chose the startx: ")
 xvalues = [10,50,100,200,300,400,500,600,800,900]
-startxcombo = ttk.Combobox(root, state="readonly", values=colors, width=10)
-startxlbl.place(relx=200, rely=400, anchor=CENTER)
-startxcombo.place(relx=250, rely=400, anchor=CENTER)
+startxcombo = ttk.Combobox(root, state="readonly", values=xvalues, width=10)
+startxlbl.place(relx=.092, rely=.8, anchor=CENTER)
+startxcombo.place(relx=.18, rely=.8, anchor=CENTER)
+
+startylbl = Label(root, text="Chose the starty: ")
+yvalues = [10,50,100,200,300,400,450]
+startycombo = ttk.Combobox(root, state="readonly", values=yvalues, width=10)
+startylbl.place(relx=0.33, rely=.8, anchor=CENTER)
+startycombo.place(relx=.42, rely=.8, anchor=CENTER)
+
+endxlbl = Label(root, text="Chose the endx: ")
+exvalues = [10,50,100,200,300,400,500,600,800,900]
+endxcombo = ttk.Combobox(root, state="readonly", values=exvalues, width=10)
+endxlbl.place(relx=0.55, rely=.8, anchor=CENTER)
+endxcombo.place(relx=.66, rely=.8, anchor=CENTER)
+
+endylbl = Label(root, text="Chose the endy: ")
+eyvalues = [10,50,100,200,300,400,450]
+endycombo = ttk.Combobox(root, state="readonly", values=eyvalues, width=10)
+endylbl.place(relx=0.77, rely=.8, anchor=CENTER)
+endycombo.place(relx=.88, rely=.8, anchor=CENTER)
 
 root.mainloop()
